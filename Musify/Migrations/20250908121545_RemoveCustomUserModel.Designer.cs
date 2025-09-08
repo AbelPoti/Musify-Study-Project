2c809398e00d0ef455768a593001265a55f4c2f1
abelke2 <potiabelke@gmail.com>
2025-09-08 15:16:40 +0300

    Create migration for previous change

--- Musify/Migrations/20250908121545_RemoveCustomUserModel.Designer.cs
+++ Musify/Migrations/20250908121545_RemoveCustomUserModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Musify.Data.DatabaseContext;
 
@@ -11,9 +12,11 @@ using Musify.Data.DatabaseContext;
 namespace Musify.Migrations
 {
     [DbContext(typeof(MusifyDbContext))]
-    partial class MusifyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250908121545_RemoveCustomUserModel")]
+    partial class RemoveCustomUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
