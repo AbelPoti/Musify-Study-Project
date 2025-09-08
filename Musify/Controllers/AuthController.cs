2a8cfe2eb149a16016a93663a69a8d0edfff94af
abelke2 <potiabelke@gmail.com>
2025-09-08 15:14:21 +0300

    Add skeleton of AuthController, register required token service and jwt usage in Program.cs

---
+++ Musify/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Musify.Services;
+
+namespace Musify.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ITokenService _tokenService;
+
+        public AuthController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, ITokenService tokenService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _tokenService = tokenService;
+        }
+    }
+}
