8a3b1926855c6c0c89d0a99caf3908e01e8d309f
abelke2 <potiabelke@gmail.com>
2025-09-09 10:47:49 +0300

    Introduce login and register actions

--- Musify/Controllers/AuthController.cs
+++ Musify/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+
+using Musify.Dtos;
 using Musify.Services;
 
 namespace Musify.Controllers
@@ -17,6 +19,63 @@ namespace Musify.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
             _tokenService = tokenService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register([FromBody] RegisterDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager.FindByNameAsync(dto.Username);
+            if (user != null)
+            {
+                return BadRequest(new { Message = "Username already taken" });
+            }
+
+            user = new IdentityUser { UserName = dto.Username, Email = dto.Email };
+            var result = await _userManager.CreateAsync(user, dto.Password);
+
+            if (result.Succeeded)
+            {
+                // Default role assignment
+                var token = _tokenService.GenerateToken(user, ["User"]);
+
+                return Ok(new { Message = "User registered successfully", token });
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] LoginDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager.FindByNameAsync(dto.Username);
+            if (user == null)
+            {
+                return Unauthorized(new { Message = "Invalid username or password" });
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, dto.Password, false);
+            if (!result.Succeeded)
+            {
+                return Unauthorized(new { Message = "Invalid username or password" });
+            }
+
+            var roles = await _userManager.GetRolesAsync(user);
+            var token = _tokenService.GenerateToken(user, roles);
+            return Ok(new { Message = "Login successful", token });
         }
     }
 }
