3360a7629a3e702c47c208d53d8e2578614e24f9
abelke2 <potiabelke@gmail.com>
2025-09-09 10:58:42 +0300

    Apply validation attributes to login and register dtos

--- Musify/Dtos/LoginDto.cs
+++ Musify/Dtos/LoginDto.cs
@@ -5,9 +5,12 @@ namespace Musify.Dtos
     public class LoginDto
     {
         [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters long")]
         public required string Username { get; set; }
 
         [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 100 characters long")]
         public required string Password { get; set; }
     }
 }
--- Musify/Dtos/RegisterDto.cs
+++ Musify/Dtos/RegisterDto.cs
@@ -5,12 +5,18 @@ namespace Musify.Dtos
     public class RegisterDto
     {
         [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters long")]
+        [RegularExpression("^[a-zA-Z0-9_-]+$", ErrorMessage = "Username can only contain letters, numbers, hyphens and underscores")]
         public required string Username { get; set; }
 
         [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [StringLength(254, ErrorMessage = "Email must be less than 255 characters")]
         public required string Email { get; set; }
 
         [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 100 characters long")]
         public required string Password { get; set; }
     }
 }
