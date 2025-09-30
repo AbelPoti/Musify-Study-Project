7a8776ba4117d9ef8bb405caf8bce1f08a2e0ab1
abelke2 <potiabelke@gmail.com>
2025-09-30 17:12:37 +0300

    Add DTOs related to password reset

---
+++ Musify/Dtos/ResetPasswordDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Musify.Dtos
+{
+    public class ResetPasswordDto
+    {
+        [Required]
+        public required string UserId { get; set; }
+
+        [Required]
+        public required string Token { get; set; }
+
+        [Required]
+        public required string NewPassword { get; set; }
+    }
+}
