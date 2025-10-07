fb559cc1cbe266bcb028df201f1ff2b4358af7ee
abelke2 <potiabelke@gmail.com>
2025-10-07 20:10:43 +0300

    Add Update Dto as well for ShopItems

---
+++ Musify/Dtos/ShopItemUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Musify.Dtos
+{
+    public class ShopItemUpdateDto
+    {
+        public int Id { get; set; }
+
+        public int InstrumentId { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+        [Required]
+        public required string Condition { get; set; }
+    }
+}
