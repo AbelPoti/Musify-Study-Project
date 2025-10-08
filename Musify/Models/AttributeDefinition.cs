b0da3f8235beb6d2f66287ee928abbc192c13410
abelke2 <potiabelke@gmail.com>
2025-10-08 11:51:45 +0300

    Add definition of attribute and value for starting out the EAV-based dynamic attribute modeling for instruments

---
+++ Musify/Models/AttributeDefinition.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Musify.Models
+{
+    public class AttributeDefinition
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string DataType { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [Required]
+        public required Category Category { get; set; }
+    }
+}
---
+++ Musify/Models/InstrumentAttributeValue.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Musify.Models
+{
+    public class InstrumentAttributeValue
+    {
+        public int Id { get; set; }
+
+        public int InstrumentId { get; set; }
+
+        [Required]
+        public required Instrument Instrument { get; set; }
+
+        public int AttributeDefinitionId { get; set; }
+
+        [Required]
+        public required AttributeDefinition AttributeDefinition { get; set; }
+
+        public required string Value { get; set; }
+    }
+}
