936828ed35812dccf0f2d49cb34efbf15d08bbce
abelke2 <potiabelke@gmail.com>
2025-10-09 15:14:46 +0300

    Create an AD update dto as well

---
+++ Musify/Dtos/AttributeDefinitionDtos/AttributeDefinitionUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Musify.Dtos.AttributeDefinitionDtos
+{
+    public class AttributeDefinitionUpdateDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string DataType { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
