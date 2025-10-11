18516828e940e528047f442580f3a1677f81357e
abelke2 <potiabelke@gmail.com>
2025-10-11 11:06:25 +0300

    Put a logger into the previously created null/validator filter

--- Musify/Filters/ValidateModelAttribute.cs
+++ Musify/Filters/ValidateModelAttribute.cs
@@ -5,16 +5,24 @@ namespace Musify.Filters
 {
     public class ValidateModelAttribute : ActionFilterAttribute
     {
+        private readonly ILogger<ValidateModelAttribute> _logger;
+        public ValidateModelAttribute(ILogger<ValidateModelAttribute> logger)
+        {
+            _logger = logger;
+        }
+
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             if (context.ActionArguments.Any(a => a.Value == null))
             {
+                _logger.LogWarning($"Request body is null in request context> {context}");
                 context.Result = new BadRequestObjectResult("Request body cannot be null.");
                 return;
             }
 
             if (!context.ModelState.IsValid)
             {
+                _logger.LogWarning($"Model state is invalid: {context.ModelState}");
                 context.Result = new BadRequestObjectResult(context.ModelState);
             }
         }
