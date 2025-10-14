be4cbb5a290833b7534fd7e77d8d832e87676ad0
abelke2 <potiabelke@gmail.com>
2025-10-14 19:38:07 +0300

    Create new Dtos for read operations

---
+++ Musify/Dtos/AttributeValueDtos/InstrumentAttributeValueReadDetailedDto.cs
@@ -0,0 +1,17 @@
+﻿using Musify.Dtos.AttributeDefinitionDtos;
+
+namespace Musify.Dtos.AttributeValueDtos
+{
+    public class InstrumentAttributeValueReadDetailedDto
+    {
+        public int Id { get; set; }
+
+        public int InstrumentId { get; set; }
+
+        public int AttributeDefinitionId { get; set; }
+
+        public required AttributeDefinitionReadMinimalDto AttributeDefinition { get; set; }
+
+        public required string Value { get; set; }
+    }
+}
