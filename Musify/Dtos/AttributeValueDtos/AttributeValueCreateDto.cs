069336cc610c33ee35b85e9a13bd7caa9325a898
abelke2 <potiabelke@gmail.com>
2025-10-14 16:49:18 +0300

    Create Dtos for Attribute Value modifications

---
+++ Musify/Dtos/AttributeValueDtos/AttributeValueCreateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Musify.Dtos.AttributeValueDtos
+{
+    public class AttributeValueCreateDto
+    {
+        public int InstrumentId { get; set; }
+
+        public int AttributeDefinitionId { get; set; }
+
+        public required string Value { get; set; }
+    }
+}
