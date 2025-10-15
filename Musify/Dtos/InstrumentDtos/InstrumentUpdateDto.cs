1dd27e8fc88bbfb53560980bf0b80a7108fdb2d9
abelke2 <potiabelke@gmail.com>
2025-10-15 12:25:30 +0300

    Add new Dtos for Instrument

---
+++ Musify/Dtos/InstrumentDtos/InstrumentUpdateDto.cs
@@ -0,0 +1,22 @@
+﻿using Musify.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Musify.Dtos.InstrumentDtos
+{
+    public class InstrumentUpdateDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string Brand { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string? Description { get; set; }
+
+        public ICollection<InstrumentAttributeValue> CustomAttributes { get; set; } = [];
+    }
+}
