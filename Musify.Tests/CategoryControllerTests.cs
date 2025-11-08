6b2df9b215d2f3669f4ce1fed2a85d00060898ab
abelke2 <potiabelke@gmail.com>
2025-11-08 17:48:35 +0200

    Add CategoryControllerTests class

---
+++ Musify.Tests/CategoryControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Musify.Controllers;
+using Musify.Models;
+using Musify.Data.DatabaseContext;
+
+namespace Musify.Tests
+{
+    [TestFixture]
+    public class CategoryControllerTests
+    {
+        private MusifyDbContext _dbContext;
+        private CategoriesController _categoryController;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Use a unique name per test class (or test) to isolate data between tests
+            var options = new DbContextOptionsBuilder<MusifyDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _dbContext = new MusifyDbContext(options);
+
+            SeedDatabase();
+
+            _categoryController = new CategoriesController(_dbContext);
+        }
+
+        public void SeedDatabase()
+        {
+            _dbContext.Categories.AddRange(
+                new Category { Id = 1, Name = "Drums and Percussion", ParentId = null },
+                new Category { Id = 2, Name = "Guitars and Basses", ParentId = null },
+                new Category { Id = 3, Name = "Acoustic Drumkits", ParentId = 1 },
+                new Category { Id = 4, Name = "Snare Drums", ParentId = 1 },
+                new Category { Id = 5, Name = "Brass Snare Drums", ParentId = 4 }
+            );
+
+            _dbContext.SaveChanges();
+        }
+
+
+        [TearDown]
+        public void Teardown()
+        {
+            _dbContext.Database.EnsureDeleted();
+            _dbContext.Dispose();
+        }
+    }
+}
