9519de27cb2050e21d4cad8059c898a1b7e89ac0
abelke2 <potiabelke@gmail.com>
2026-02-10 18:47:41 +0200

    #7 Introduce first basic filter dtos

---
+++ Musify/Dtos/RequestDtos/FilterDtos/InstrumentFiterDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Musify.Dtos.RequestDtos.FilterDtos
+{
+    public record InstrumentFiterDto
+    {
+        [MaxLength(256)]
+        public string? Name { get; init; }
+        
+        [MaxLength(128)]
+        public string? Brand { get; init; }
+        
+        // TODO Create filtering based on categories
+    }
+}
---
+++ Musify/Dtos/RequestDtos/FilterDtos/ShopItemFilterDto.cs
@@ -0,0 +1,13 @@
+﻿using Musify.Models;
+
+namespace Musify.Dtos.RequestDtos.FilterDtos
+{
+    public record ShopItemFilterDto
+    {
+        public InstrumentFiterDto? InstrumentFiter { get; init; }
+        
+        public PriceFilterDto? PriceFilter { get; init; }
+        
+        public string? Condition { get; init; }
+    }
+}
